17e1d75b381b92bc1d320da98010e13bdffecbf4
Kenton Blacutt <kenton@blacutt.com>
2019-04-11 14:35:41 +0800

    SingleUserChat

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,11 +2,18 @@ class RoomChannel < ApplicationCable::Channel
 
   def subscribed
     stream_from "room_channel"
+    ActionCable.server.broadcast 'room_channel',
+      conversation: current_user.conversations.last.id,
+      online: true
+    s = current_user.conversations.last
+    s.online = true
+    s.save
   end
 
   def unsubscribed
     s = current_user.conversations.last
     s.online = false
+    s.save
     ActionCable.server.broadcast 'room_channel',
       conversation: current_user.conversations.last.id,
       online: false
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,22 +5,38 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @conversation = current_user.conversations.last
-    message = @conversation.messages.build(message_params)
-    message.user_id = current_user.id
-    message.save
+    unless current_user.admin
+      @conversation = current_user.conversations.last
+      message = @conversation.messages.build(message_params)
+      message.user_id = current_user.id
+      message.save
+
       if @conversation.messages.count == 1
         ActionCable.server.broadcast 'room_channel',
                 activated: true,
                 content:  message.content,
                 user: current_user.id,
+                conversation: @conversation.id,
                 username: current_user.username
       else
         ActionCable.server.broadcast 'room_channel',
                 content:  message.content,
                 user: current_user.id,
+                conversation: @conversation.id,
                 username: current_user.username
       end
+    else
+      @conversation = Conversation.find(params[:conversation_id])
+      message = @conversation.messages.build(message_params)
+      message.user_id = current_user.id
+      if message.save
+        ActionCable.server.broadcast 'room_channel',
+          content:  message.content,
+          user: current_user.id,
+          conversation: @conversation.id,
+          username: current_user.username
+      end
+    end
   end
 
   private
@@ -28,14 +44,6 @@ class MessagesController < ApplicationController
     def get_messages
       @messages = Message.for_display
       @message  = current_user.messages.build
-    end
-
-    def logged_in_user
-      @current_user = User.new
-      @current_user.password = 'password'
-      @current_user.password_confirmation = 'password'
-      @current_user.username = "Anónimo"
-      @current_user.save
     end
 
     def message_params
